b51de8465d1e41907fc57232d3b9ed142a7f1fd3
localleon <me@lrau.xyz>
2025-02-21 18:34:41 +0100

    Adding external-dns to flux

--- infra/01-bootstrap.sh
+++ infra/01-bootstrap.sh
@@ -1,3 +1,13 @@
+# Update FluxCD manifests with current infrastructure setup! 
+userAssignedIdentityID=$(az identity list --query "[?name=='aks-dev0-external-dns-identity'].id" -o tsv)       
+sed -i "s|\(azure.workload.identity/client-id:\s*\).*|\1$userAssignedIdentityID|" infra/fluxcd-apps/external-dns.yaml
+
+# Commit and push changes to the repository
+git add infra/fluxcd-apps/external-dns.yaml
+git commit -m "Update external-dns identity"
+git push
+
+# Bootstrap FluxCD 
 flux bootstrap github \
   --token-auth \
   --owner=localleon \
@@ -5,3 +15,4 @@ flux bootstrap github \
   --branch=main \
   --path=infra/fluxcd \
   --personal
+
